b7ecf1c3fa210fe4a85c22bdb14535451bbcae0b
bgar324 <bentgarcia05@gmail.com>
2024-08-14 22:10:27 -0700

    Fixed projects wording

--- src/Pages/2Projects/AppProjects.jsx
+++ src/Pages/2Projects/AppProjects.jsx
@@ -13,7 +13,7 @@ const AppProjects = () => {
       </Helmet>
       <div className="projects">
       <Projects
-        link="https://www.youtube.com"
+        link="https://benjamingarcia.vercel.app/"
         imageSrc="/static/images/portfolio-image.png"
         title="Personal Portfolio"
         description="Check out my portfolio and let me know what you think! I built it using React JS, Vite, and CSS to showcase my projects and my skills. My goal was to create a platform with the focus of networking and connecting with others. I'm excited to network and explore new opportunities together, so feel free to reach out!"
@@ -24,7 +24,7 @@ const AppProjects = () => {
         link="https://github.com/bgar324/suika"
         imageSrc="/static/images/suika.png"
         title="Suika Remake in Python"
-        description="asfsfd"
+        description="Remake of the popular Suika Watermelon game coded entirely in Python. I used Pymunk for realistic physics like collisions, gravity, and friction, and Pygame for rendering graphics and game development. Python handled the game logic, including checking if balls level up when contacting similar ones."
         languages={['Python','Pygame','Pymunk']}
       />
 
@@ -32,8 +32,8 @@ const AppProjects = () => {
         link="https://beautiful-gumption-a0ca0e.netlify.app/"
         imageSrc="/static/images/weather.png"
         title="Real Time Weather"
-        description="asdfs"
-        languages={['HTML', 'CSS', 'JavaScript', 'OpeanWeather API']}
+        description="Responsive front-end application that retrieves live weather data from OpenWeather's API. Features include real-time updates, location-based forecasts, and detailed metrics like humidity, wind speed, and temperature extremes. Offering a intuitive user interface with a clean design optimized for both desktop and mobile devices."
+        languages={['HTML', 'CSS', 'JavaScript', 'OpenWeather API']}
       />
       </div>
     </>
